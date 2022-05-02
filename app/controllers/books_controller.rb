81cc6bf1e088007f0ceff6b1158d0d9b97572df8
kiiiiichan <k3i4k6k7u@gmail.com>
2022-05-02 13:51:42 +0000

    Edit Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,16 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/show'
   end
 
   def index
+    @books = Book.all
   end
 
   def show
@@ -19,4 +24,11 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
